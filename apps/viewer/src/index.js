03c910f2b174d2a741694ed6b313025ef4d8e2b0
Fanaen <contact@fanaen.fr>
2019-05-27 23:00:51 +0200

    feat(viewer): add a canvas and a basic button to hide it

--- apps/viewer/src/index.js
+++ apps/viewer/src/index.js
@@ -8,3 +8,29 @@ for (let i = 0; i < 1000; i++) {
 const t1 = performance.now();
 
 console.log("Result: " + result + " (" + ((t1 - t0)/1000) + " millisecondes)");
+
+// Test the canvas
+const canvas = document.getElementById('web-tracing-canvas');
+if (canvas.getContext) {
+    const ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
+}
+
+// -- UX --
+// Hide button
+const webtracingDisplay = document.getElementById('web-tracing-display');
+const hideButton = document.getElementById('close-web-tracing');
+
+const webtracingDisplayClass = webtracingDisplay.className;
+let isWebtracingDisplayVisible = true;
+
+hideButton.onclick = () => {
+    isWebtracingDisplayVisible = !isWebtracingDisplayVisible;
+    webtracingDisplay.className = webtracingDisplayClass + (isWebtracingDisplayVisible ? '' : ' hidden');
+    hideButton.className = isWebtracingDisplayVisible ? 'btn fas fa-minus' : 'btn fas fa-plus'
+};
