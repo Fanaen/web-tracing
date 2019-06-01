4fbaf6038717888b265988f89860434037e80530
Fanaen <contact@fanaen.fr>
2019-06-02 00:16:23 +0200

    feat(viewer): add loader and textual feedback

--- apps/viewer/src/wasm-module.js
+++ apps/viewer/src/wasm-module.js
@@ -4,6 +4,19 @@ import axios from 'axios';
 let useWasm = true;
 const serverUrl = 'http://localhost:8000';
 
+function logDrawIn(str) {
+    console.log(str);
+    document.getElementById('draw-in').innerText = str;
+}
+function logAppliedIn(str) {
+    console.log(str);
+    document.getElementById('applied-in').innerText = str;
+}
+
+const webLoadingDisplay = document.getElementById('web-tracing-display');
+function setLoading(active) {
+    webLoadingDisplay.className = active ? 'loading' : '';
+}
 
 function draw(ctx, width, height, cameraPos, cameraRotation, cameraFov) {
     if (useWasm) {
@@ -11,12 +24,15 @@ function draw(ctx, width, height, cameraPos, cameraRotation, cameraFov) {
         const cameraPosVector = wasm.Vector3.new(cameraPos.x, cameraPos.y, cameraPos.z);
         const cameraRotationVector = wasm.Vector3.new(cameraRotation.x, cameraRotation.y, cameraRotation.z);
 
+        setLoading(true);
         const before = performance.now();
         wasm.draw(ctx, width, height, cameraPosVector, cameraRotationVector, cameraFov);
         const after = performance.now();
-        console.log(`Draw in ${(after - before).toFixed(3)}ms`);
+        setLoading(false);
+        logDrawIn(`Draw in ${(after - before).toFixed(3)}ms`);
     } else {
         // Call the server
+        setLoading(true);
         axios.post(`${serverUrl}/draw`, {
             width,
             height,
@@ -29,7 +45,7 @@ function draw(ctx, width, height, cameraPos, cameraRotation, cameraFov) {
             },
         })
         .then(response => {
-            console.log(`Draw in ${response.data.time}`);
+            logDrawIn(`Draw in ${response.data.time}`);
 
             const before = performance.now();
             const imageData = ctx.createImageData(width, height);
@@ -41,7 +57,8 @@ function draw(ctx, width, height, cameraPos, cameraRotation, cameraFov) {
 
             ctx.putImageData(imageData, 0, 0, 0, 0, width, height);
             const after = performance.now();
-            console.log(`Applied to canvas in ${(after - before).toFixed(3)}ms`);
+            setLoading(false);
+            logAppliedIn(`Applied to canvas in ${(after - before).toFixed(3)}ms`);
 
         })
         .catch(error => console.error('Error when calling draw:', error)); // Print the error if one occurred;
