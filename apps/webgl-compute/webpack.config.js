c4dc7543de50a7900c677f5816416561c4fe893a
Kevin Masson <kevin.masson@methodinthemadness.eu>
2019-08-04 11:58:43 +0200

    feat(gpu): package the gpu app with webpack

---
+++ apps/webgl-compute/webpack.config.js
@@ -0,0 +1,17 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+const dist = path.resolve(__dirname, "dist");
+
+module.exports = [
+    {
+        entry: './src/app.js',
+        output: {
+            path: dist,
+            filename: 'app.js',
+        },
+        mode: 'development',
+        plugins: [
+            new CopyWebpackPlugin(['src/index.html'])
+        ],
+    }]
